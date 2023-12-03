ac78b8e37094e117bf5dade0b98e39418509329d
Biruk <Birukabayneh2@gmail.com>
2023-12-03 22:45:27 +0300

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        l = len(sentence)
+        f = sentence[0]
+        return (l, f)
+
---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a = "Hello"
+print(a[0])
