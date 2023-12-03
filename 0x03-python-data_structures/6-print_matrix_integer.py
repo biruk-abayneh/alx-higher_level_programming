3391318e482cee1dc836e1cc3767e8b9118742e3
biruk-abayneh <birukabayneh2@gmail.com>
2023-12-03 10:54:59 -0800

    36

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):    
-    if matrix == None:
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
         print("")
     else:
         for i in matrix:
